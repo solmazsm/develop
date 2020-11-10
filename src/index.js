805bf8e78f22dcd5db3e6d3b3f517d0200ac27da
Solmaz Seyed Monir <55103176+solmazsm@users.noreply.github.com>
2020-11-10 15:12:03 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ ReactDOM.render(
   
  <Router>
     <Switch>
- <BrowserRouter basename='./'>
+ <BrowserRouter basename={window.location.pathname || ''}>
       <Route exact path="/">
         <Navi />
         <Text />
